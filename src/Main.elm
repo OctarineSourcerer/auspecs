9da5cc6808f6629e4f700d87c0e093b9a3b3f4f7
Dan Gomme <daniel.gomme@yahoo.co.uk>
2018-05-04 14:51:47 +0100

    Modular sheet fields system, minimal display of all fields in a model

--- src/Main.elm
+++ src/Main.elm
@@ -1,30 +1,83 @@
 import Html exposing (..)
+import Html.Attributes exposing (..)
 import Model exposing (..)
+import Dict exposing (..)
+import Html.Events exposing (..)
+import String exposing(..)
 
+main : Program Never { fields : Dict String Field } Msg
 main = 
   Html.beginnerProgram { model = model, view = view, update = update }
-  
 
 
 -- Update
-type Msg = Modify
-
+type Msg 
+  = ChangeField Field
 
 update : Msg -> Model -> Model
 update msg model = 
   case msg of
-    Modify -> model
-
+    ChangeField f ->
+      { model | fields = Dict.insert f.name f model.fields }
 
 -- View
 
-textList things =
-  List.map (\el -> div [] [text el]) things
+{-| Create HTML elements to view and edit the named Field in the `model`.
+  They should give a name, and a control.
+  Should the named Field not exist, you get a giant banner saying "No `fieldName` in model"
+-}
+
+fieldView model name =
+  let
+    modelField = get name model.fields
+
+    inp kind val event newRec =
+      input [ type_ kind, event newRec, value val ] []
+
+    fromInput field f =
+      (\x -> ChangeField { field | value = f x })
+
+    inputField field = 
+      case field.value of
+        String_ s -> 
+          inp "text" s onInput (fromInput field String_)
+
+        Int_ i    -> 
+          inp "number" (toString i) onInput (fromInput field (String.toInt >> Result.withDefault i >> Int_)) 
+       
+        Bool_ b   -> 
+          inp "checkbox" (toString b) onCheck (fromInput field Bool_)
+          
+  in
+    case modelField of
+      -- If it's not in the model, indicate this LOUDLY. Done cos models have to use maps for now, so field access not safe
+      Nothing ->
+        div [ nopeStyle ] [ text ("No " ++ name ++ " field in model") ]
+
+      Just f  ->
+        div [] 
+          [ text name
+          , inputField f ]
+
+
+{-| A naive view that will display every field in the model. Field views are placed in a div.
+  However, it will not give you control over the layout - mostly to be used for dev purposes.
+-}
+naiveView : Model -> Html Msg
+naiveView model =
+  div [] <| List.map (fieldView model) (Dict.keys model.fields)
 
 view : Model -> Html Msg
 view model =
-  div [] <|
-    textList 
-      [ model.name 
-      , (toString model.cursed)
-      , (toString model.ac)]
+  naiveView model
+
+nopeStyle : Attribute msg
+nopeStyle =
+  style
+    [ ("width", "100%")
+    , ("padding", "10px 0")
+    , ("font-size", "2em")
+    , ("text-align", "center")
+    , ("background-color", "#ff3333")
+    , ("color", "#e60000")
+    ]
--- src/Model.elm
+++ src/Model.elm
@@ -1,13 +1,31 @@
 module Model exposing (..)
+import Dict exposing (..)
 
--- Model
-type alias Model = 
+-- Supported field types
+type FieldType 
+  = String_ String
+  | Int_ Int
+  | Bool_ Bool
+
+type alias Field = 
   { name : String
-  , cursed : Bool 
-  , ac : Int }
+  , description : Maybe String 
+  , value : FieldType 
+  }
+
+-- Model. Have to do this with map for now, as a custom model would require a different message type for each diff field
+-- However, CHANGE this if you end up being containing how to get/set within in a message. Lenses?
+type alias Model = 
+  { fields : Dict String Field
+  }
 
 
 model = 
-  { name = "Dan"
-  , cursed = False
-  , ac = 10 }
+  let
+    fieldEntry name desc val =
+      (name, Field name desc val)
+  in
+    { fields = Dict.fromList 
+        [ fieldEntry "Name" Nothing (String_ "Dan") 
+        , fieldEntry "Cursed" (Just "Whether your character is cursed") (Bool_ True)
+        , fieldEntry "AC" (Just "How hard it is to hit your character") (Int_ 10)]}
